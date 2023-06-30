b24a4c6c26d12ea915599ed3ebc1d1bbb99d4ac2
Eshaan Barkataki <39027204+Eshaancoding@users.noreply.github.com>
2023-06-30 18:13:29 -0400

    new route

--- src/routes/left.cpp
+++ src/routes/left.cpp
@@ -38,51 +38,39 @@ void Routes::left() {
         pros::delay(10);
         if (effectors.shouldReset == false) break;
     }
-    
+
     // spin roller    
     drive.goForward(4_in, {{0, 1}}, {{0.5, [](){
         effectors.outtake();
     } }} );
-    effectors.delay(100_ms);
+    effectors.delay(300_ms);
     effectors.stopIntake();
-    drive.goBackward(6_in);
+    drive.goBackward(8_in);
     
-    // go to middle point
-    drive.turnLeft(130_deg, {{0, 0.75}});
-    drive.setToleranceParams(std::nullopt, std::nullopt, 3_s);
-    drive.goForward(2.6_tile, {{0, 1}});
-    drive.resetToleranceParams();
-    
-    // shoot disk!
-    drive.turnRight(85_deg);
-    effectors.enablePiston();
-    drive.goBackward(3_in, {}, {{0.5, [](){
-        effectors.shoot();
-    }}});
+    // spin to face goal
+    drive.turnLeft(15_deg); 
+
+    // go forward & shoot
+    drive.goBackward(4_ft); 
     effectors.delay(100_ms);
-    effectors.disablePiston();
+    effectors.shoot();
+    effectors.delay(100_ms);
 
-    // exit out of shooting position and turn and go to other roller
-    drive.goForward(7_in);
-    drive.turnLeft(83_deg, {{0, 0.75}});
-    drive.setToleranceParams(std::nullopt, std::nullopt, 3_s);
-    drive.goForward(2.55_tile, {{0, 1}});
-    drive.resetToleranceParams();
+    // go backward and face roller
+    drive.goForward(1_ft); // make sure that at this point it is aligned with the y-pos of the roller
+    drive.turnRight(105_deg); // this should now have the roller wheels aligned with the roller
 
-    // go forward a little to touch roller
-    drive.turnRight(40_deg);
-    drive.setToleranceParams(std::nullopt, std::nullopt, 1.5_s);
-    drive.goForward(7_in, {{0, 0.75}} );
-    drive.resetToleranceParams();
-
-    // spin the roller
-    effectors.outtake();
-    effectors.delay(550_ms);
+    // go forward until you reach roller
+    drive.goForward(10_ft, {{0, 1}}, {{0.5, [](){
+        effectors.outtake();
+    } }} );
+    effectors.delay(300_ms);
     effectors.stopIntake();
+    drive.goBackward(10_ft);
 
-    // expand
-    drive.goBackward(4_in);
-    drive.turnLeft(45_deg);
+    // now do expansion
+    drive.turnRight(135_deg);
+    drive.goForward(2_ft);
     effectors.expand();
     effectors.expand(false);
 
