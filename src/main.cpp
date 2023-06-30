051e39e638c2efbe968735411706da3c4a5fa68d
Eshaan Barkataki <39027204+Eshaancoding@users.noreply.github.com>
2023-06-30 18:44:11 -0400

    main yay

--- src/main.cpp
+++ src/main.cpp
@@ -44,63 +44,79 @@ void initialize() {
 void autonomous() {
     leftMotorGroup.setBrakeMode(AbstractMotor::brakeMode::brake);
     rightMotorGroup.setBrakeMode(AbstractMotor::brakeMode::brake);
+    Routes::left();
 };
 
 // Operation control (driver)
 void opcontrol() {
-    leftMotorGroup.setBrakeMode(AbstractMotor::brakeMode::coast);
-    rightMotorGroup.setBrakeMode(AbstractMotor::brakeMode::coast);
 
-    bool isEnabled = false;
-    effectors.disablePiston();
+    // uncomment all of this out if you drive 
+    // comment the uaton code (scroll down)
 
-    while (true) {
-        double analogLeft = Control::getAnalog(E_CONTROLLER_ANALOG_LEFT_Y);
-        double analogRight = Control::getAnalog(E_CONTROLLER_ANALOG_RIGHT_Y);
+    // ******************** DRIVE CODE ***************
+    // leftMotorGroup.setBrakeMode(AbstractMotor::brakeMode::coast);
+    // rightMotorGroup.setBrakeMode(AbstractMotor::brakeMode::coast);
 
-        double l = odometery.getLeftRevs();
-        double r = odometery.getRightRevs();
-        Console::printBrain(1, "left %.3f right %.3f", l, r);
+    // bool isEnabled = false;
+    // effectors.disablePiston();
 
-        drive.moveTank(analogLeft, analogRight);
+    // while (true) {
+    //     double analogLeft = Control::getAnalog(E_CONTROLLER_ANALOG_LEFT_Y);
+    //     double analogRight = Control::getAnalog(E_CONTROLLER_ANALOG_RIGHT_Y);
 
-        if (Control::getButtonPressed(E_CONTROLLER_DIGITAL_L1)) {
-            effectors.shoot();
-        }
+    //     double l = odometery.getLeftRevs();
+    //     double r = odometery.getRightRevs();
+    //     Console::printBrain(1, "left %.3f right %.3f", l, r);
 
-        if (Control::getButtonPressed(E_CONTROLLER_DIGITAL_R1)) {
-            effectors.intake(127);
-        }
-        else if (Control::getButtonPressed(E_CONTROLLER_DIGITAL_R2)) {
-            effectors.outtake(127);
-        }
-        else {
-            effectors.stopIntake();
-        }
+    //     drive.moveTank(analogLeft, analogRight);
 
-        if (Control::getDebouncePressed(pros::E_CONTROLLER_DIGITAL_UP)) {
-            isEnabled = !isEnabled;
-            if (isEnabled) {
-                Control::printController(0, "Piston Enabled  ");
-                effectors.enablePiston();
-            } else {
-                Control::printController(0, "Piston Disabled");
-                effectors.disablePiston();
-            }
-        }
+    //     if (Control::getButtonPressed(E_CONTROLLER_DIGITAL_L1)) {
+    //         effectors.shoot();
+    //     }
 
-        if (Control::getDebouncePressed(pros::E_CONTROLLER_DIGITAL_LEFT)) {
-            effectors.expand();
-        }
+    //     if (Control::getButtonPressed(E_CONTROLLER_DIGITAL_R1)) {
+    //         effectors.intake(127);
+    //     }
+    //     else if (Control::getButtonPressed(E_CONTROLLER_DIGITAL_R2)) {
+    //         effectors.outtake(127);
+    //     }
+    //     else {
+    //         effectors.stopIntake();
+    //     }
 
-        if (Control::getDebouncePressed(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
-            effectors.expand(false);
-        }
+    //     if (Control::getDebouncePressed(pros::E_CONTROLLER_DIGITAL_UP)) {
+    //         isEnabled = !isEnabled;
+    //         if (isEnabled) {
+    //             Control::printController(0, "Piston Enabled  ");
+    //             effectors.enablePiston();
+    //         } else {
+    //             Control::printController(0, "Piston Disabled");
+    //             effectors.disablePiston();
+    //         }
+    //     }
 
-        effectors.stepCataReset();
+    //     if (Control::getDebouncePressed(pros::E_CONTROLLER_DIGITAL_LEFT)) {
+    //         effectors.expand();
+    //     }
 
-        Console::printBrain(0, odometery.getPos(), "Pos");
+    //     if (Control::getDebouncePressed(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
+    //         effectors.expand(false);
+    //     }
+
+    //     effectors.stepCataReset();
+
+    //     Console::printBrain(0, odometery.getPos(), "Pos");
         
-        pros::delay(10);
-    }
+    //     pros::delay(10);
+    // }
+
+
+
+    // uncomment all of this out if you auton 
+    // comment the drive code (scroll up)
+
+    // ******************** AUTON CODE ***************
+    leftMotorGroup.setBrakeMode(AbstractMotor::brakeMode::brake);
+    rightMotorGroup.setBrakeMode(AbstractMotor::brakeMode::brake);
+    Routes::left();
 }
