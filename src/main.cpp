ce9d1072cc3e0e952d677672b55c7af83dda2cce
Eshaan Barkataki <39027204+Eshaancoding@users.noreply.github.com>
2023-07-25 11:06:23 -0400

    arcade shenanagins

--- src/main.cpp
+++ src/main.cpp
@@ -49,74 +49,15 @@ void autonomous() {
 
 // Operation control (driver)
 void opcontrol() {
-
-    // uncomment all of this out if you drive 
-    // comment the uaton code (scroll down)
-
     // ******************** DRIVE CODE ***************
-    // leftMotorGroup.setBrakeMode(AbstractMotor::brakeMode::coast);
-    // rightMotorGroup.setBrakeMode(AbstractMotor::brakeMode::coast);
+    leftMotorGroup.setBrakeMode(AbstractMotor::brakeMode::coast);
+    rightMotorGroup.setBrakeMode(AbstractMotor::brakeMode::coast);
 
-    // bool isEnabled = false;
-    // effectors.disablePiston();
+    while (true) {
+        double analogLeft = Control::getAnalog(E_CONTROLLER_ANALOG_LEFT_Y);
+        double analogRight = Control::getAnalog(E_CONTROLLER_ANALOG_RIGHT_X);
 
-    // while (true) {
-    //     double analogLeft = Control::getAnalog(E_CONTROLLER_ANALOG_LEFT_Y);
-    //     double analogRight = Control::getAnalog(E_CONTROLLER_ANALOG_RIGHT_Y);
+        drive.moveArcade(analogLeft, analogRight);
 
-    //     double l = odometery.getLeftRevs();
-    //     double r = odometery.getRightRevs();
-    //     Console::printBrain(1, "left %.3f right %.3f", l, r);
-
-    //     drive.moveTank(analogLeft, analogRight);
-
-    //     if (Control::getButtonPressed(E_CONTROLLER_DIGITAL_L1)) {
-    //         effectors.shoot();
-    //     }
-
-    //     if (Control::getButtonPressed(E_CONTROLLER_DIGITAL_R1)) {
-    //         effectors.intake(127);
-    //     }
-    //     else if (Control::getButtonPressed(E_CONTROLLER_DIGITAL_R2)) {
-    //         effectors.outtake(127);
-    //     }
-    //     else {
-    //         effectors.stopIntake();
-    //     }
-
-    //     if (Control::getDebouncePressed(pros::E_CONTROLLER_DIGITAL_UP)) {
-    //         isEnabled = !isEnabled;
-    //         if (isEnabled) {
-    //             Control::printController(0, "Piston Enabled  ");
-    //             effectors.enablePiston();
-    //         } else {
-    //             Control::printController(0, "Piston Disabled");
-    //             effectors.disablePiston();
-    //         }
-    //     }
-
-    //     if (Control::getDebouncePressed(pros::E_CONTROLLER_DIGITAL_LEFT)) {
-    //         effectors.expand();
-    //     }
-
-    //     if (Control::getDebouncePressed(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
-    //         effectors.expand(false);
-    //     }
-
-    //     effectors.stepCataReset();
-
-    //     Console::printBrain(0, odometery.getPos(), "Pos");
-        
-    //     pros::delay(10);
-    // }
-
-
-
-    // uncomment all of this out if you auton 
-    // comment the drive code (scroll up)
-
-    // ******************** AUTON CODE ***************
-    leftMotorGroup.setBrakeMode(AbstractMotor::brakeMode::brake);
-    rightMotorGroup.setBrakeMode(AbstractMotor::brakeMode::brake);
-    Routes::left();
+    } 
 }
--- src/routes/left.cpp
+++ src/routes/left.cpp
@@ -6,28 +6,6 @@
 #include "Console.h"
 
 void Routes::left() {
-    /**
-     * Hi priya, a few things/tips:
-
-     * use goForward, goBackward, turnLeft, or turnRight. Don't use goToPoint, etc. Has weird coordinate system that's pretty weird
-
-     * factor map: basically changes the how fast/slow the robot goes. there's two params. The first one determines what percentage of the path the factor should activate
-        * ex:, if the first param is 0 and the second param is 0.5, then the the start of the path it will go 50% speed
-        * ex: if the first param is 0.25 and the second param is 0.75, then at 25% of the path it will start to go 75% speed until the path is done
-
-     * callback map: basically calls a function after reaches a certain % of path
-        * ex: if the first param is 0.5 and the second is effectors.outtake(), then at 50% of the path it will start calling the effectors.outtake() functon (only called once)
-        * note that if you want to have only a callback map and not a factor map, then you may have to do something like goForward(2_in, {}, {{0.5, func()}}). The {} in the middle indicates empty factor map (i.e factor stays the same: 1)
-
-     * effectors.enablePiston() = enable piston boost (shoots disk with more power). DisablePiston does the opposite
-
-     * also note that sometimes the angles are weird, you may have to tune them. (for example 83 degrees is really suppose to be 90 but odom can mess up pretty easily so 83 is there to compensate for that)
-
-     * if the robot oscillates a little, then maybe adding a decreased factor (i.e make the robot go at 75% speed by adding {{0, 0.75}} to the factor map parameter)
-
-     * GL :) 
-     */
-
     // effectors.disablePiston();
     // // sets position
     // odometery.setPos({14_in, 108_in, 180_deg});
