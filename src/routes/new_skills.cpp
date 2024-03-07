4a5da74bdcb244111dc2b280399926ff13dab648
Eshaan Barkataki <eshaanbarkataki@gmail.com>
2024-03-06 20:47:07 -0500

    sdf

--- src/routes/new_skills.cpp
+++ src/routes/new_skills.cpp
@@ -16,9 +16,9 @@ void Routes::new_skills () {
     macro(true);
 
     // ========= get the 4 middle triballs =======
-    drive.goForward(1.5_tile);
+    drive.goForward(1.5_tile, {{0, 1.2}});
     drive.turnRight(180_deg);
-    drive.goBackward(0.8_tile);
+    drive.goBackward(0.8_tile, {{0, 1.2}});
     drive.faceToPoint({10_tile, -1_tile}, true, {{0, 1.2}});
 
     eff.wingsPistonRight.set_value(1);
@@ -31,7 +31,7 @@ void Routes::new_skills () {
 
     // ========== Realign and go under bar ======
     drive.faceToPoint({0_in, -10_tile}, true);
-    drive.goForward(2_tile, {{0, 1.3}});
+    drive.goForward(2_tile, {{0, 1.5}});
 
     drive.setToleranceParams(nullopt, nullopt, 0.75_s);
     drive.turnLeft(45_deg);
@@ -71,7 +71,7 @@ void Routes::new_skills () {
     }, 8_in, 6_in, true, 4.5_s);
 
     eff.wingsPistonRight.set_value(0);
-    drive.faceToPoint({-15_tile, -40_tile}, true);
+    drive.faceToPoint({-15_tile, -40_tile}, true, {{0, 1.3}});
     eff.wingsPistonRight.set_value(1);
     drive.moveArcade(-0.7, 0);
     pros::delay(1200);
@@ -92,7 +92,7 @@ void Routes::new_skills () {
         Path({1.3_tile, -0.8_tile}),
     }, 8_in, 6_in, false, 4_s);
 
-    drive.faceToPoint({30_tile, -50_tile}, true);
+    drive.faceToPoint({30_tile, -50_tile}, true, {{0, 1.3}});
     
     eff.wingsPistonLeft.set_value(1);
     eff.wingsPistonRight.set_value(1);
@@ -103,28 +103,29 @@ void Routes::new_skills () {
     eff.wingsPistonRight.set_value(0);
 
     // ============== very Right ==============
+    OdomCustom::setPos(0_in, 0_in, -180_deg);
+    
     drive.goForward(1_tile);
 
     drive.setToleranceParams(nullopt, nullopt, 0.5_s);
     drive.faceToPoint({-30_tile, -10_tile}, true);
     drive.resetToleranceParams();
     
+    eff.wingsPistonLeft.set_value(1);
     drive.goPath({
-        Path({0_in, 0_in}, 1.2, 1.2),
-        Path({1.5_tile, 0.5_tile}, 1.2, 1.2),
-        Path({1.5_tile, 1_tile}, 1.2, 1.2, 0.3_tile),
-        Path({1_tile, 2_tile}, 1.2, 1.2),
-        Path({0_tile, 2_tile}, 1.2, 1.2),
+        Path({0_in, 0_in}, 0.4, 1.2),
+        Path({1.65_tile, 1_tile}),
+        Path({1.65_tile, 1.5_tile}),
+        Path({1.5_tile, 1.75_tile}),
     }, 8_in, 12_in, true, 5_s);
 
-    drive.moveArcade(1, 0);
+    drive.faceToPoint({40_tile, -30_tile}, true, {{0, 1.3}});
+    eff.wingsPistonLeft.set_value(0);
+    drive.moveArcade(0.5, 0);
     pros::delay(200);
+    drive.moveArcade(-1, -0.3);
+    pros::delay(1200);
+    drive.moveArcade(1, 0.5);
+    pros::delay(500);
     drive.moveArcade(0, 0);
-
-    // drive.faceToPoint({10_tile * 1, 10_tile * -1.25}, true);
-    // drive.moveArcade(-1, 0);
-    // pros::delay(1200);
-    // drive.moveArcade(1, 0);
-    // pros::delay(600);
-    // drive.moveArcade(0, 0);
 }
