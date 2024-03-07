a20072e40ad8303e362e03f16f041cd5771b33c4
Eshaan Barkataki <eshaanbarkataki@gmail.com>
2024-03-07 09:20:18 -0500

    sdfsdfsdf

--- src/routes/skills.cpp
+++ src/routes/skills.cpp
@@ -59,6 +59,7 @@ void Routes::skills () {
 
     macro();
 
+    // ===================== Go under ==============
     drive.goForward(4_in);
     drive.faceToPoint({-30_tile, -30_tile}, true);
 
@@ -71,39 +72,88 @@ void Routes::skills () {
         Path({-0.4_tile, 5_tile}),
     }, 8_in, 10_in, true);
 
-    drive.moveArcade(-1, 0);
-    pros::delay(1000);
-    drive.moveArcade(0.7, 0);
-    pros::delay(250);
-    drive.faceToPoint({20_tile, -5_tile}, true);
-    drive.moveArcade(-1, 0);
-    pros::delay(1000);
-    drive.moveArcade(0, 0);
+    // ==================== The rest of the slam movement ===========
 
-    // curved path (this is for hitting triball)
-    OdomCustom::setPos(0_in, 0_in);
+    drive.moveArcade(0.5, 0);
+    pros::delay(200);
+    drive.faceToPoint({-10_tile, 0_tile}, true);
+    drive.moveArcade(-1, 0);
+    pros::delay(400);
+    drive.moveArcade(0.5, 0);
+    pros::delay(200);
+    drive.moveArcade(0, 0);
+    drive.faceToPoint({-0_tile, 10_tile}, true);
+
     drive.goPath({
-        Path({0_tile, 0_tile}, 1, 0.8, 0.3_tile),
-        Path({0.4_tile, -0.4_tile}, 1, 0.8, 0.3_tile),
-        Path({0_tile, -0.8_tile}, 1.3, 1.3),
-        Path({-2_tile, -1.75_tile})
-    }, 8_in, 6_in);
+        Path({0_in, 0_in}, 1, 0.8, 0.3_tile),
+        Path({0_in, -1.2_tile}, 1, 0.8, 0.3_tile, [](){
+            eff.wingsPistonRight.set_value(1);
+        }),
+        Path({0_in, -1.35_tile}, 1, 0.8, 0.3_tile, [](){
+            eff.wingsPistonLeft.set_value(1);
+        }),
+        Path({-0.5_tile, -1.65_tile}, 1, 0.8, 0.3_tile),
+        Path({-0.85_tile, -1.73_tile}, 1, 0.8, 0.3_tile),
+        Path({-1.2_tile, -1.8_tile}, 1, 0.8, 0.3_tile)
+    }, 8_in, 6_in, true, 4.5_s);
 
-    drive.setToleranceParams(nullopt, nullopt, 0.75_s);
-    drive.faceToPoint({0_tile, 20_tile}, true);
-    drive.resetToleranceParams();
-
-    // slam
-    eff.wingsToggle();
-    drive.moveArcade(1, 0);
+    eff.wingsPistonRight.set_value(0);
+    drive.faceToPoint({15_tile, -40_tile}, true, {{0, 1.3}});
+    eff.wingsPistonRight.set_value(1);
+    drive.moveArcade(-0.7, 0);
     pros::delay(1200);
-    drive.moveArcade(-1, 0);
-    pros::delay(600);
     drive.moveArcade(1, 0);
-    pros::delay(1000);
+    pros::delay(400);
     drive.moveArcade(-1, 0);
     pros::delay(600);
+    drive.moveArcade(0.5, 0);
+    pros::delay(300);
     drive.moveArcade(0, 0);
-    eff.wingsToggle();
+
+    // ============== Right ==============
+    eff.wingsPistonLeft.set_value(0);
+    eff.wingsPistonRight.set_value(0);
+    drive.goPath({
+        Path({0_in, 0_in}),
+        Path({0_in, -0.8_tile}),
+        Path({-1.3_tile, -0.8_tile}),
+    }, 8_in, 6_in, false, 4_s);
+
+    drive.faceToPoint({-30_tile, -50_tile}, true, {{0, 1.3}});
+    
+    eff.wingsPistonLeft.set_value(1);
+    eff.wingsPistonRight.set_value(1);
+    drive.moveArcade(-1, 0);
+    pros::delay(1200);
+    drive.moveArcade(0, 0);
+    eff.wingsPistonLeft.set_value(0);
+    eff.wingsPistonRight.set_value(0);
+
+    // ============== very Right ==============
+    OdomCustom::setPos(0_in, 0_in, -180_deg);
+    
+    drive.goForward(1_tile);
+
+    drive.setToleranceParams(nullopt, nullopt, 0.5_s);
+    drive.faceToPoint({30_tile, -10_tile}, true);
+    drive.resetToleranceParams();
+    
+    eff.wingsPistonLeft.set_value(1);
+    drive.goPath({
+        Path({0_in, 0_in}, 0.4, 1.2),
+        Path({-1.65_tile, 1_tile}),
+        Path({-1.65_tile, 1.5_tile}),
+        Path({-1.5_tile, 1.75_tile}),
+    }, 8_in, 12_in, true, 5_s);
+
+    drive.faceToPoint({-40_tile, -30_tile}, true, {{0, 1.3}});
+    eff.wingsPistonLeft.set_value(0);
+    drive.moveArcade(0.5, 0);
+    pros::delay(200);
+    drive.moveArcade(-1, -0.3);
+    pros::delay(1200);
+    drive.moveArcade(1, 0.5);
+    pros::delay(500);
+    drive.moveArcade(0, 0);
     
 }
