f0184d08bf5ccdbe8febd90b08dfc19f6dd9f499
mahuan16 <mariahuan2008@gmail.com>
2024-09-12 15:44:22 -0400

    Routes!

--- src/routes/skills.cpp
+++ src/routes/skills.cpp
@@ -8,9 +8,9 @@
 
 void Routes::skills () {
     OdomCustom::setPos(0_in, 0_in, 90_deg);
-    eff.intakeToggle();
+    eff.intakeToggle(); 
     pros::delay(1000);
-    eff.intakeToggle();
+    eff.intakeToggle(); //intake off
 
     drive.goPath({ // REALLY REALLY REALLY TUNABLE!!!
         Path({0_in, 0_in}),
@@ -20,7 +20,7 @@ void Routes::skills () {
 
     drive.faceToPoint({3_tile, 0_in}, true);
     drive.goBackward(1_tile);
-    eff.toggleClamp();
+    eff.toggleClamp(); //clamp on
     drive.goPath({
         Path({0_in, 0_in}),
         Path({1_tile, -0.5_tile}),
@@ -34,35 +34,71 @@ void Routes::skills () {
         Path({-1_tile, 0.5_tile}),
         Path({-2.5_tile, 0.5_tile})
     });
+    
 
     drive.faceToPoint({0.5_tile, -0.5_tile}, true);
     drive.goForward(0.707_tile);
     drive.faceToPoint({1_tile, 0.5_tile}, true);
     drive.goBackward(1.118_tile);
-    eff.toggleClamp();
+    eff.toggleClamp(); //clamp off
 
     //second part skills auton
-    drive.goForward(4.472_tile);
-    drive.faceToPoint({2_tile, 0_tile}, true);
+    drive.faceToPoint({1.75_tile, 3.75_tile});
     eff.intakeToggle();
+    drive.goForward(4.472_tile);
+    pros::delay(500); // tunable 
+    eff.intakeToggle();
+
+    drive.faceToPoint({2_tile, 0_tile}, true);
     drive.goBackward(1_tile);
     eff.toggleClamp();
     eff.intakeToggle();
+
+    // 
     drive.goPath({
+        Path({0_in, 0_in}),
         Path({1_tile, 0.3_tile}),
         Path({1.75_tile, 0.75_tile}),
         Path({2_tile, 1.5_tile})
     });
 
+    drive.turnLeft(180_deg);
     drive.goPath({
+        Path({0_in, 0_in}),
         Path({-1_tile, -0.5_tile}),
         Path({-2.5_tile, -0.5_tile})
     });
 
     drive.faceToPoint({0.5_tile, 0.5_tile}, true);
     drive.goForward(0.707_tile);
-    drive.faceToPoint({1_tile, -0.5_tile}, true);
+    drive.faceToPoint({3_tile, -0.5_tile}, true);
     drive.goBackward(1.118_tile);
     eff.toggleClamp();
 
+    //third part of skills auton
+    eff.intakeToggle(); //delete if intake alr on
+    drive.goForward(4.123_tile);
+    pros::delay(1500); //let ring go on intake
+    eff.intakeToggle(); //turn off
+    drive.faceToPoint({0_tile, -1_tile}, true);
+    eff.intakeToggle(); //turn on
+    drive.goForward(1_tile);
+    pros::delay(1000);
+    eff.intakeToggle();
+    drive.faceToPoint({-1_tile, 1_tile}, true);
+    drive.goBackward(1.414_tile);
+    eff.toggleClamp(); //clamp on
+    eff.intakeToggle(); //turn on, put rings on mogo
+    drive.faceToPoint({-1_tile, 1_tile}, true);
+    drive.goForward(1.414_tile);
+    drive.faceToPoint({0_tile, -1_tile}, true);
+    drive.goForward(1_tile);
+    drive.faceToPoint({2_tile, 0_tile}, true);
+    drive.goForward(1.5_tile);
+    drive.faceToPoint({-0.5_tile, 1_tile}, true);
+    drive.goBackward(1.12_tile);
+    eff.toggleClamp(); //clamp off, mogo in corner
+    eff.intakeToggle(); //intake off
+    drive.faceToPoint({0_tile, 1_tile}, true);
+    drive.goForward(5_tile); //slam mogo in corner
 }
